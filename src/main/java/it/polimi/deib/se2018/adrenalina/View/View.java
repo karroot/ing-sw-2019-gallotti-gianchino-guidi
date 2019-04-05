f1b21b8c987a937fbeec532665488983273fbe27
karroot <gabbo97pop@hotmail.it>
2019-04-05 21:37:16 +0200

    Creation of View class

---
+++ src/main/java/it/polimi/deib/se2018/adrenalina/View/View.java
@@ -0,0 +1,89 @@
+package it.polimi.deib.se2018.adrenalina.View;
+
+import it.polimi.deib.se2018.adrenalina.Model.ColorId;
+import it.polimi.deib.se2018.adrenalina.Model.Player;
+
+
+public class View
+{
+
+
+    private ColorId playerOfRound;
+
+
+    public void showMenu()
+    {
+
+    }
+
+    public void newGame()
+    {
+
+    }
+
+    public void showBoard()
+    {
+
+    }
+
+    public void startRound( Player playofround)
+    {
+
+    }
+
+    public void showPowerUp()
+    {
+
+    }
+
+    public void selectPowerUp()
+    {
+
+    }
+
+    public void showWeapons()
+    {
+
+    }
+
+    public void selectWeapon()
+    {
+
+    }
+
+    public void showAction()
+    {
+
+    }
+
+    public void selectAction()
+    {
+
+    }
+
+    public void startFrenesy()
+    {
+
+    }
+
+    public void showFinalScore()
+    {
+
+    }
+
+    public void showMessage(String message)
+    {
+
+    }
+
+    public void showError(String message)
+    {
+
+    }
+
+    public void showPlayerBoard()
+    {
+
+    }
+
+}
