d5afdfdff53986b67f1abc00673042bfeb53597f
Giovanni <giovanniguidi@icloud.com>
2019-04-23 14:42:34 +0200

    implementati gli statetypes con documentazione

--- src/main/java/it/polimi/deib/se2018/adrenalina/Model/Adrenalized1.java
+++ src/main/java/it/polimi/deib/se2018/adrenalina/Model/Adrenalized1.java
@@ -1,6 +1,14 @@
 package it.polimi.deib.se2018.adrenalina.Model;
 
+import it.polimi.deib.se2018.adrenalina.Model.weapon_cards.WeaponCard;
+
 import java.util.*;
+
+/**
+ * This class implements the adrenalized1 status of a player.
+ *
+ * @author giovanni
+ */
 
 public class Adrenalized1 implements StatePlayer
 {
@@ -10,8 +18,9 @@ public class Adrenalized1 implements StatePlayer
     {
 
     }
+
     /**
-     * When the player is in the adrenalized1 status he can reach Square to a max distance of 3. This method make the player see all the possible reachable squares.
+     * When the player is in the normal status he can reach Square to a max distance of 3. This method make the player see all the possible reachable squares.
      *
      * @param player is the player that is looking where to move
      * @param gameBoard is needed to call the squareReachableNoWall function
@@ -20,12 +29,13 @@ public class Adrenalized1 implements StatePlayer
     @Override
     public Set<Square> lookForRunAround(Player player, GameBoard gameBoard)
     {
-        return  GameBoard.getArena().squareReachableNoWall(player.getSquare().getX(), player.getSquare().getY(), 3);
-
+        Set<Square>  squareSet;
+        squareSet = gameBoard.getArena().squareReachableNoWall(player.getSquare().getX(), player.getSquare().getY(), 3);
+        return squareSet;
     }
 
     /**
-     * When the player is in the Adrenalized1 status he can reach Square to grab stuffs with a max distance of 2. This method make the player see all the possible reachable squares.
+     * When the player is in the normal status he can reach Square to grab stuffs with a max distance of 1. This method make the player see all the possible reachable squares.
      *
      * @param player is the player that is looking where to move to grab the stuff
      * @param gameBoard is needed to call the squareReachableNoWall function
@@ -35,36 +45,72 @@ public class Adrenalized1 implements StatePlayer
     public Set<Square> lookForGrabStuff(Player player, GameBoard gameBoard)
     {
         Set<Square> squareSet;
-        squareSet = GameBoard.getArena().squareReachableNoWall(player.getSquare().getX(), player.getSquare().getY(), 2);
+        squareSet = gameBoard.getArena().squareReachableNoWall(player.getSquare().getX(), player.getSquare().getY(), 2);
         return squareSet;
     }
 
+
     /**
-     * it return the reachable player for shooting , checking the square in room that the player can see
+     * This method will return a list of all the players a player can see. It's useful when you have to check who you can shoot.
+     *
+     * @param player is the player looking for shoot
+     * @param gameBoard is needed to call the squareReachableNoWall function
+     *
+     * @return a list of player the player called this method can see
      */
     @Override
     public List<Player> lookForShootPeople(Player player, GameBoard gameBoard)
     {
+        List<Player> playerList = null;
 
-        for (Square varSquare : GameBoard.getArena().squareReachableNoWall(player.getSquare().getX(), player.getSquare().getY(), 1)) //for every square returned by squareReachableNoWall with distance 1
+        playerList.addAll(player.getSquare().getRoom().getPlayerRoomList()); //adds all the players in the room
+
+        /*
+         * Now i have to check if the player is close to a door. In this case i can see all the players in this adiacent room.
+         * I will implement the method in this way:
+         * 1. I check if the player can move to a different room with a distance 1 using the method squareReachableNoWall
+         * 2. If the player can effectively move to a different room it means it is close to a door
+         * 3. I will add to the list all the players in this different room
+         */
+
+        for (Square square : gameBoard.getArena().squareReachableNoWall(player.getSquare().getX(), player.getSquare().getY(), 1))
         {
-            if (!(player.getSquare().getColor().equals(varSquare.getColor()))) // if the color of the reachable square is different from the color of the square where the player is this means player can see in a different room
+            if (!(player.getSquare().getColor().equals(square.getColor()))) // if the color of the reachable square is different from the color of the square
+            // where the player is this means player can see in a different room
             {
-                for (Square tempSquare : varSquare.getRoom().getSquareList()) //for every square in this different room
-                {
-                    return tempSquare.getPlayerList(); //return the playerlist of those squares
-                }
+                playerList.addAll(square.getRoom().getPlayerRoomList()); //adds all the players in this room
             }
 
-            else return varSquare.getPlayerList();
         }
 
 
-        return null;
+        return playerList;
     }
-    public void checkReload()
-    {
 
+
+    /**
+     * This method will check all the reloadable weapons of a player. After i choose to reload a weapon i must reiterate this method.
+     *
+     * @param player is the player checking is weapon
+     * @return a list of reloadable weapons
+     */
+    @Override
+    public List<WeaponCard> checkReload(Player player)
+    {
+        List<WeaponCard> reloadableWeapons = null; //This is the weaponcard list i will return
+        /*
+         * For each weapon i check if the player has enough ammo to reload that weapon.
+         * After a player choose a weapon to reload i have to delete the ammo from the player and reiterate this method.
+         */
+        for (WeaponCard weaponCard: player.getWeaponCardList())
+        {
+            if (weaponCard.getBlueAmmoCost()<=player.getAmmoBlue() && weaponCard.getRedAmmoCost()<=player.getAmmoRed() && weaponCard.getYellowAmmoCost()<=player.getAmmoYellow())
+            {
+                reloadableWeapons.add(weaponCard);
+            }
+        }
+
+        return reloadableWeapons;
     }
 
 }
--- src/main/java/it/polimi/deib/se2018/adrenalina/Model/Adrenalized2.java
+++ src/main/java/it/polimi/deib/se2018/adrenalina/Model/Adrenalized2.java
@@ -1,7 +1,16 @@
 package it.polimi.deib.se2018.adrenalina.Model;
 
+import it.polimi.deib.se2018.adrenalina.Model.weapon_cards.WeaponCard;
+
 import java.util.List;
 import java.util.Set;
+
+/**
+ * This class implements the adrenalized2 status of a player.
+ *
+ * @author giovanni
+ */
+
 
 public class Adrenalized2 implements StatePlayer
 {
@@ -21,43 +30,89 @@ public class Adrenalized2 implements StatePlayer
     @Override
     public Set<Square> lookForRunAround(Player player, GameBoard gameBoard)
     {
-        return  GameBoard.getArena().squareReachableNoWall(player.getSquare().getX(), player.getSquare().getY(), 3);
-
+        Set<Square>  squareSet;
+        squareSet = gameBoard.getArena().squareReachableNoWall(player.getSquare().getX(), player.getSquare().getY(), 3);
+        return squareSet;
     }
 
+    /**
+     * When the player is in the normal status he can reach Square to grab stuffs with a max distance of 1. This method make the player see all the possible reachable squares.
+     *
+     * @param player is the player that is looking where to move to grab the stuff
+     * @param gameBoard is needed to call the squareReachableNoWall function
+     * @return a set of possible reachable squares with max distance 1
+     */
+    @Override
     public Set<Square> lookForGrabStuff(Player player, GameBoard gameBoard)
     {
-        return  GameBoard.getArena().squareReachableNoWall(player.getSquare().getX(), player.getSquare().getY(), 2);
-
-
+        Set<Square> squareSet;
+        squareSet = gameBoard.getArena().squareReachableNoWall(player.getSquare().getX(), player.getSquare().getY(), 2);
+        return squareSet;
     }
+
+
     /**
-     * it return the reachable player for shooting , checking the square in room that the player can see
+     * This method will return a list of all the players a player can see. It's useful when you have to check who you can shoot.
+     *
+     * @param player is the player looking for shoot
+     * @param gameBoard is needed to call the squareReachableNoWall function
+     *
+     * @return a list of player the player called this method can see
      */
     @Override
     public List<Player> lookForShootPeople(Player player, GameBoard gameBoard)
     {
-        //GameBoard.getArena().squareReachableNoWall(Player.getSquare().getX(), Player.getSquare().getY(), 1); the controller module will lunch LookForAround with distance 1
+        List<Player> playerList = null;
 
+        playerList.addAll(player.getSquare().getRoom().getPlayerRoomList()); //adds all the players in the room
 
-        for (Square varSquare : GameBoard.getArena().squareReachableNoWall(player.getSquare().getX(), player.getSquare().getY(), 1)) //for every square returned by squareReachableNoWall with distance 1
+        /*
+         * Now i have to check if the player is close to a door. In this case i can see all the players in this adiacent room.
+         * I will implement the method in this way:
+         * 1. I check if the player can move to a different room with a distance 1 using the method squareReachableNoWall
+         * 2. If the player can effectively move to a different room it means it is close to a door
+         * 3. I will add to the list all the players in this different room
+         */
+
+        for (Square square : gameBoard.getArena().squareReachableNoWall(player.getSquare().getX(), player.getSquare().getY(), 1))
         {
-            if (!(player.getSquare().getColor().equals(varSquare.getColor()))) // if the color of the reachable square is different from the color of the square where the player is this means player can see in a different room
+            if (!(player.getSquare().getColor().equals(square.getColor()))) // if the color of the reachable square is different from the color of the square
+            // where the player is this means player can see in a different room
             {
-                for (Square tempSquare : varSquare.getRoom().getSquareList()) //for every square in this different room
-                {
-                    return tempSquare.getPlayerList(); //return the playerlist of those squares
-                }
-            } else return varSquare.getPlayerList();
+                playerList.addAll(square.getRoom().getPlayerRoomList()); //adds all the players in this room
+            }
+
         }
 
 
-        return null;
+        return playerList;
     }
+    //todo in this case the player can also move before to shot. The controller will have to call the squareReachableNoWall
+    // with distance 1, and then see who the player can see
 
-    public void checkReload()
+    /**
+     * This method will check all the reloadable weapons of a player. After i choose to reload a weapon i must reiterate this method.
+     *
+     * @param player is the player checking is weapon
+     * @return a list of reloadable weapons
+     */
+    @Override
+    public List<WeaponCard> checkReload(Player player)
     {
+        List<WeaponCard> reloadableWeapons = null; //This is the weaponcard list i will return
+        /*
+         * For each weapon i check if the player has enough ammo to reload that weapon.
+         * After a player choose a weapon to reload i have to delete the ammo from the player and reiterate this method.
+         */
+        for (WeaponCard weaponCard: player.getWeaponCardList())
+        {
+            if (weaponCard.getBlueAmmoCost()<=player.getAmmoBlue() && weaponCard.getRedAmmoCost()<=player.getAmmoRed() && weaponCard.getYellowAmmoCost()<=player.getAmmoYellow())
+            {
+                reloadableWeapons.add(weaponCard);
+            }
+        }
 
+        return reloadableWeapons;
     }
 
 }
