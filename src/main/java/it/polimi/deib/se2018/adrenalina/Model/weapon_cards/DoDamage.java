39a1c12169c809133a3a93f18e8369103f102e28
Francesco Gianchino <42522213+cysko7927@users.noreply.github.com>
2019-05-03 20:54:45 +0200

    Inizio Implementazione Classe shotgun

--- src/main/java/it/polimi/deib/se2018/adrenalina/Model/weapon_cards/DoDamage.java
+++ src/main/java/it/polimi/deib/se2018/adrenalina/Model/weapon_cards/DoDamage.java
@@ -1,11 +1,13 @@
 package it.polimi.deib.se2018.adrenalina.Model.weapon_cards;
 
+import it.polimi.deib.se2018.adrenalina.Model.Player;
+
 import java.util.*;
 
 public interface DoDamage
 {
 
 
-    public void doDamage();
+    public void doDamage(Player player,int quantity);
 
 }
